7414e821f12a39c52141e700c41d12d854d5c206
Forrest Gump <trung.development@gmail.com>
2024-04-04 21:19:46 +0200

    Install Laravel Jetstream

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/version', [GitController::class, 'getGitObjectsTimestamp'])->name('version');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
